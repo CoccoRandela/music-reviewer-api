202d69fe1d5569a63e603cc79f6c89b2e6effdba
CoccoRandela <rocco.candela94@gmail.com>
2023-07-14 10:33:20 +0200

    Adds timestamps and unique user + album

--- src/models/review.ts
+++ src/models/review.ts
@@ -7,27 +7,34 @@ interface IReview {
 	text?: string;
 }
 
-const reviewSchema = new Schema<IReview>({
-	userId: {
-		type: Schema.Types.ObjectId,
-		required: true,
+const reviewSchema = new Schema<IReview>(
+	{
+		userId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+		},
+		albumId: {
+			type: String,
+			required: true,
+		},
+		score: {
+			type: Number,
+			required: true,
+			min: 10,
+			max: 100,
+		},
+		text: {
+			type: String,
+			minlength: 10,
+			maxlength: 1000,
+		},
 	},
-	albumId: {
-		type: String,
-		required: true,
-	},
-	score: {
-		type: Number,
-		required: true,
-		min: 10,
-		max: 100,
-	},
-	text: {
-		type: String,
-		minlength: 10,
-		maxlength: 1000,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
+
+reviewSchema.index({ userId: 1, albumId: 1 }, { unique: true });
 
 const Review = model('Review', reviewSchema);
 
