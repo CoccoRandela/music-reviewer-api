d4ac927dd038abe4c54f9a96b076f41538c220db
CoccoRandela <rocco.candela94@gmail.com>
2023-07-20 19:51:34 +0200

    fixes types

--- src/controllers/reviews.ts
+++ src/controllers/reviews.ts
@@ -1,5 +1,5 @@
 import { IReview, Review } from '../models/review';
-import { AtLeastOnePropertyOf } from '../utils/types';
+import { AtLeastOnePropertyOf } from '../utils/types/AtLeastOne';
 import createHttpError from 'http-errors';
 import { NextFunction, Request, Response } from 'express';
 
